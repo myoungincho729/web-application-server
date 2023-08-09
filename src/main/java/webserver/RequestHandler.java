ed5ee6eff9faa50d987ca9944ef4990c3d452d49
myoungin <myoungin729@gmail.com>
2023-08-09 16:53:34 +0900

    feat: RequestHandler 내에서 HTTP 메시지 파싱 구현

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,13 +1,13 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.Marker;
 
 public class RequestHandler extends Thread {
 	private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -21,15 +21,46 @@ public class RequestHandler extends Thread {
 	public void run() {
 		log.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(), connection.getPort());
 		
-		try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
+		try (InputStream in = connection.getInputStream();
+			 OutputStream out = connection.getOutputStream();
+			 BufferedReader br = new BufferedReader(new InputStreamReader(in))) {
 			// TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
 			DataOutputStream dos = new DataOutputStream(out);
-			byte[] body = "Hello World".getBytes();
-			response200Header(dos, body.length);
-			responseBody(dos, body);
+
+			// 헤더와 라인 구별
+			HttpHeader httpHeader = parseHeader(br);
+			System.out.println(httpHeader.toString());
+			if (httpHeader.getUri().equals("/favicon.ico")) {
+				return;
+			}
+			BufferedReader brf = new BufferedReader(new FileReader("./webapp" + httpHeader.getUri()));
+
+			String content;
+			String body = "";
+			while ((content = brf.readLine()) != null) {
+				body += content + '\n';
+			}
+			if (body.equals("")) {
+				body = "Hello world";
+			}
+
+			byte[] body2 = body.getBytes();
+			response200Header(dos, body2.length);
+			responseBody(dos, body2);
 		} catch (IOException e) {
 			log.error(e.getMessage());
 		}
+	}
+
+	private HttpHeader parseHeader(BufferedReader br) throws IOException {
+		List<String> headerStrings = new ArrayList<>();
+		String line = "";
+		while (true) {
+			line = br.readLine();
+			if (line.length() == 0) break;
+			headerStrings.add(line);
+		}
+		return HttpHeader.from(headerStrings);
 	}
 
 	private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
