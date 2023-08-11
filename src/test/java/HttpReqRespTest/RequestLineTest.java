38d2af589e5cabfea5b33b765f11613b48bd47a8
myoungin <myoungin729@gmail.com>
2023-08-11 15:26:18 +0900

    test: 요청 라인 테스트코드 추가

---
+++ src/test/java/HttpReqRespTest/RequestLineTest.java
@@ -0,0 +1,32 @@
+package HttpReqRespTest;
+
+import org.junit.Assert;
+import org.junit.Test;
+import refactoring.RequestLine;
+
+public class RequestLineTest {
+
+    @Test
+    public void create_method() {
+        RequestLine line = new RequestLine("GET /index.html HTTP/1.1");
+        Assert.assertEquals("GET", line.getMethod());
+        Assert.assertEquals("/index.html", line.getPath());
+        Assert.assertEquals("HTTP/1.1", line.getVersion());
+
+        line = new RequestLine("POST /index.html HTTP/1.1");
+        Assert.assertEquals("POST", line.getMethod());
+        Assert.assertEquals("/index.html", line.getPath());
+        Assert.assertEquals("HTTP/1.1", line.getVersion());
+    }
+
+    @Test
+    public void withParameter() {
+        RequestLine line = new RequestLine("GET /create?name=myoungin&password=1234 HTTP/1.1");
+        Assert.assertEquals("GET", line.getMethod());
+        Assert.assertEquals("/create", line.getPath());
+        Assert.assertEquals("HTTP/1.1", line.getVersion());
+        Assert.assertEquals("myoungin", line.getParams().get("name"));
+        Assert.assertEquals("1234", line.getParams().get("password"));
+
+    }
+}
