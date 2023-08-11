3d1b56a26e7767e3bb00be5a30e89e47e329fa51
myoungin <myoungin729@gmail.com>
2023-08-11 15:11:31 +0900

    refactor: 요청라인 파싱부분을 별도의 메서드로 분리

--- src/main/java/refactoring/HttpRequest.java
+++ src/main/java/refactoring/HttpRequest.java
@@ -1,5 +1,7 @@
 package refactoring;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import util.HttpRequestUtils;
 import util.IOUtils;
 
@@ -11,44 +13,56 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class HttpRequest {
+    private static final Logger log = LoggerFactory.getLogger(HttpRequest.class);
     private String method;
     private String path;
     private String version;
-    private Map<String, String> headers;
-    private Map<String, String> parameters;
+    private Map<String, String> headers = new HashMap<>();
+    private Map<String, String> parameters = new HashMap<>();
     private String body;
 
     public HttpRequest(InputStream in) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(in));
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
 
-        String[] firstLine = br.readLine().split(" ");
-        method = firstLine[0];
+            String line = br.readLine();
+            if (line == null) {
+                return;
+            }
+            processRequestLine(line);
 
-        parameters = new HashMap<>();
-        int index = firstLine[1].indexOf("?");
+            line = br.readLine();
+            while (!line.equals("")) {
+                log.debug("header : {}", line);
+                String[] tokens = line.split(":");
+                headers.put(tokens[0].trim(), tokens[1].trim());
+                line = br.readLine();
+            }
+
+            if ("POST".equals(method)) {
+                body = IOUtils.readData(br, Integer.valueOf(headers.get("Content-Length")));
+                if (getHeader("Content-Type").equals("application/x-www-form-urlencoded")) {
+                    parameters = HttpRequestUtils.parseQueryString(body);
+                }
+            }
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+
+    private void processRequestLine(String requestLine) {
+        String[] reqLineParts = requestLine.split(" ");
+
+        method = reqLineParts[0];
+
+        int index = reqLineParts[1].indexOf("?");
         if (index == -1) {
-            path = firstLine[1];
+            path = reqLineParts[1];
         } else {
-            path = firstLine[1].substring(0, index);
-            parameters = HttpRequestUtils.parseQueryString(firstLine[1].substring(index + 1));
+            path = reqLineParts[1].substring(0, index);
+            parameters = HttpRequestUtils.parseQueryString(reqLineParts[1].substring(index + 1));
         }
-        version = firstLine[2];
-
-        headers = new HashMap<>();
-        while (true) {
-            String header = br.readLine();
-            if (header.isEmpty()) {
-                break;
-            }
-            String[] headerKeyVal = header.split(": ");
-            headers.put(headerKeyVal[0], headerKeyVal[1]);
-        }
-        if (headers.containsKey("Content-Length")) {
-            body = IOUtils.readData(br, Integer.valueOf(headers.get("Content-Length")));
-            if (getHeader("Content-Type").equals("application/x-www-form-urlencoded")) {
-                parameters = HttpRequestUtils.parseQueryString(body);
-            }
-        }
+        version = reqLineParts[2];
     }
 
     public String getMethod() {
