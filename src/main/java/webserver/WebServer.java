2aee8f40ba9ae7645910d1dc45b8039f9a9bad94
myoungin <myoungin729@gmail.com>
2023-08-13 18:50:04 +0900

    fix: UserRepository대신 Database static 메서드 쓰도록 변경

--- src/main/java/webserver/WebServer.java
+++ src/main/java/webserver/WebServer.java
@@ -3,15 +3,18 @@ package webserver;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+import model.Database;
 import model.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import refactoring.RequestHandlerV6;
+
+import javax.xml.crypto.Data;
 
 public class WebServer {
 	private static final Logger log = LoggerFactory.getLogger(WebServer.class);
-	private static final int DEFAULT_PORT = 8080;
-    public static final UserRepository userRepository = new UserRepository();
-	
+	private static final int DEFAULT_PORT = 8081;
+
     public static void main(String argv[]) throws Exception {
         // 서버소켓을 생성한다. 웹서버는 기본적으로 8080번 포트를 사용한다.
     	
@@ -21,10 +24,9 @@ public class WebServer {
             // 클라이언트가 연결될때까지 대기한다.
             Socket connection;
             while ((connection = listenSocket.accept()) != null) {
-            	RequestHandler requestHandler = new RequestHandler(connection);
+            	RequestHandlerV6 requestHandler = new RequestHandlerV6(connection);
                 requestHandler.start();
                 requestHandler.join();
-                userRepository.printUsers();
             }
     	}
     }
